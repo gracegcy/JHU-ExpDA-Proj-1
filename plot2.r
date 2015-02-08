d4861dfcdcf983c72ad922b9e7276deba097221c
gracegcy <gu.chengyao.grace@gmail.com>
2015-02-08 16:43:30 -0500

    plot 23 update

--- plot2.r
+++ plot2.r
@@ -1,17 +1,7 @@
 ## loading file
 require(sqldf)
 file <- c("C:/Users/cgu018/Desktop/Analytics/Open Course/DS JHU/Explore Data Viz/Week 1/household_power_consumption.txt")
-data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file 
-where (Date == '1/2/2007' OR Date == '2/2/2007')" )
-
-## deal with the question marks in the data an change to NA
-data_subset[data_subset == "?"] <- NA 
-
-## loading file
-require(sqldf)
-file <- c("C:/Users/cgu018/Desktop/Analytics/Open Course/DS JHU/Explore Data Viz/Week 1/household_power_consumption.txt")
-data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file 
-where (Date == '1/2/2007' OR Date == '2/2/2007')" )
+data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 
 ## deal with the question marks in the data an change to NA
 data_subset[data_subset == "?"] <- NA 
@@ -20,8 +10,7 @@ data_subset[data_subset == "?"] <- NA
 data_subset$date_Time <- strptime(paste(data_subset$Date, data_subset$Time), "%d/%m/%Y %H:%M:%S")
 
 ## plot and save as png
-plot(data_subset$date_Time, data_subset$Global_active_power, type = "l", 
-     xlab="", ylab = "Global Active Power (kilowatts)")
+plot(data_subset$date_Time, data_subset$Global_active_power, type = "l", xlab="", ylab = "Global Active Power (kilowatts)")
 dev.copy(png, "plot2.png", width =480, height=480)
 dev.off()
 
--- plot3.r
+++ plot3.r
@@ -2,15 +2,6 @@
 require(sqldf)
 file <- c("C:/Users/cgu018/Desktop/Analytics/Open Course/DS JHU/Explore Data Viz/Week 1/household_power_consumption.txt")
 data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file 
-                            where (Date == '1/2/2007' OR Date == '2/2/2007')" )
-
-## deal with the question marks in the data an change to NA
-data_subset[data_subset == "?"] <- NA 
-
-## loading file
-require(sqldf)
-file <- c("C:/Users/cgu018/Desktop/Analytics/Open Course/DS JHU/Explore Data Viz/Week 1/household_power_consumption.txt")
-data_subset <- read.csv.sql(file, header = T, sep=";", sql = "select * from file 
 where (Date == '1/2/2007' OR Date == '2/2/2007')" )
 
 ## deal with the question marks in the data an change to NA
